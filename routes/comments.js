d95ed32cac613bbca077881c7e8c57535cf4971c
AcAntellAno <acantellano1996@gmail.com>
2018-08-16 15:21:49 -0700

    Added Authentication and Authorization to comments

--- routes/comments.js
+++ routes/comments.js
@@ -48,7 +48,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 //Edit Route
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
   Comment.findById(req.params.comment_id, (err, foundComment) => {
     if (err) {
       res.redirect('back');
@@ -62,12 +62,23 @@ router.get('/:comment_id/edit', (req, res) => {
 });
 
 //Update Route
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, upatedComment) => {
     if (err) {
       res.redirect('back');
     } else {
       res.redirect("/campgrounds/" + req.params.id);
+    }
+  })
+});
+
+//Destroy Route
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
     }
   })
 });
@@ -79,4 +90,25 @@ function isLoggedIn(req, res, next) {
   }
   res.redirect('/login');
 }
+
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        console.log(err);
+        res.redirect('back');
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    res.redirect('back');
+  }
+}
+
 module.exports = router;
